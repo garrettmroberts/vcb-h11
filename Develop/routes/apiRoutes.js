6161ccc943f1d1afdc2e619d1d225a710116e0b5
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-20 16:06:40 -0500

    added delete api

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,15 +1,32 @@
 var path = require("path");
 var fs = require("fs");
+var bodyParser = require("body-parser");
 
 var db = require("../db/db.json");
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     res.json(db);
-  })
+  });
 
   app.post("/api/notes", function(req, res) {
     db.push(req.body);
     res.json("Success");
-  })
-}
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    var id = req.params.id;
+
+    for (var i = 0; i < db.length; i++) {
+      if (id === db[i].id) {
+        db.splice(i, 1);
+      }
+    }
+    // db.forEach((item, index) => {
+    //   if (id === item.id) {
+    //     db = db.slice(0, index) + db.slice(index + 1);
+    //   };
+    // });
+    res.json(db);
+  });
+};
